5e56e84a92a3858c58ebf965137406a4a719a55e
HoD <henrybrownonline@yahoo.com>
2022-10-09 18:57:28 +0100

    update

--- Chapter2/guessing_game.py
+++ Chapter2/guessing_game.py
@@ -1,17 +1,28 @@
 import random
 
 random_guess = random.randint(1, 10)
+range_count = 3
 counter = 4
 count = 0
 while count <= 3:
     user_input = int(input("Enter a number btw 1 and 10: "))
 
     if user_input < 0 or user_input > 10:
-        print("Number out of range, try again!!!\n")
+        print("Number out of range, try again!!!")
+        range_count -= 1
+        if range_count == 0:
+            print("Goodbye")
+            break
+        elif range_count == 1:
+            print(f"{range_count} try left\n")
+        else:
+            print(f"{range_count} tries left\n")
         continue
+
     if user_input == random_guess:
         print("You guessed right!!!")
         break
+
     if user_input != random_guess:
         print("wrong guess!!!")
         count += 1
